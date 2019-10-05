30640db5f637aefb287b41dbe539b89f11dbfa02
Dominic Schmidli <dominicschmidli2510@gmail.com>
2019-10-05 14:09:26 +0200

    modified constructor of Leader

--- src/Leader.java
+++ src/Leader.java
@@ -2,14 +2,19 @@ import java.lang.Math;
 
 public class Leader extends Square{
     public int move;
-    int end;
+    public int end;
 
     public Leader(int start, int numberOfSquares){
+        super();
         double enddouble = Math.random();
         enddouble = enddouble *numberOfSquares + 1;
         end = (int) enddouble;
 
         move = end-start;
+    }
+
+    public int getEnd() {
+        return end;
     }
 
 }
