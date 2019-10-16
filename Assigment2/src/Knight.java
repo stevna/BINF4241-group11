2736450710d2309659c1b6e571b935d6a5ec481d
Dominic Schmidli <dominicschmidli2510@gmail.com>
2019-10-16 18:32:38 +0200

    added moveValidation method to Knight

--- Assigment2/src/Knight.java
+++ Assigment2/src/Knight.java
@@ -1,17 +1,28 @@
 public class Knight extends ChessPiece {
+    private int[] possibleMoves = { -1, 2, -1, -2, 1, 2, 1, -2, -2, 1, -2, -1, 2, 1, 2, -1 };
+
 
     public Knight(Enum col, int x, int y) {
         super();
         color = col.toString();
         xcord = x;
         ycord = y;
+
         if (color.equals("white")){
             shortName = "WN";
         }
         else if(color.equals("black")){
             shortName = "BN";
         }
-
     }
 
+    @Override
+    public boolean moveValidation(int xDest, int yDest) {
+        for (int i = 0; i < possibleMoves.length; i = i + 2) {
+            if (xDest == xcord + possibleMoves[i] && yDest == ycord + possibleMoves[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
