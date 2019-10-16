ccc5d5c9f7639850596a97d4e2d6e7314120ae4f
Dominic Schmidli <dominicschmidli2510@gmail.com>
2019-10-16 21:23:49 +0200

    modified the pawn's moveValidation with black and white distinction

--- Assigment2/src/Pawn.java
+++ Assigment2/src/Pawn.java
@@ -1,25 +1,55 @@
-import java.util.ArrayList;
-
 public class Pawn extends ChessPiece {
-    private int[] possibleMoves = {0, 1, -1, 1, 1, -1};
-    //ArrayList<Object> initialposition = new ArrayList<>();
+    private int[] possibleMovesWhite = {0, 1, -1, 1, 1, -1};
+    private int[] possibleMovesBlack = {0, -1, 1, -1, -1, 1};
+    private int[] possibleFirstMovesWhite = {0, 1, -1, 1, 1, -1, 0, 2};
+    private int[] possibleFirstMovesBlack = {0, -1, 1, -1, -1, 1, 0, -2};
+    private boolean first_move = true;
+
+
     public Pawn(Enum col, int x) {
         super();
         color = col.toString();
         xcord = x;
 
-        if (color == "white") {
+        if (color.equals("white")) {
             ycord = 1;
             shortName = "WP";
         }
-        else if (color == "black") {
+        else if (color.equals("black")) {
             ycord = 6;
             shortName = "BP";
         }
     }
 
+    private void setFirst_moveToFalse() {
+        first_move = false;
+    }
+
+    private  boolean isFirst_move() {
+        return first_move;
+    }
+
     @Override
     public boolean moveValidation(int xDest, int yDest) {
+        int[] possibleMoves;
+        if(color.equals("white")) {
+            if (isFirst_move()) {
+                possibleMoves = possibleFirstMovesWhite;
+                setFirst_moveToFalse();
+            }
+            else {
+                possibleMoves = possibleMovesWhite;
+            }
+        }
+        else {
+            if (isFirst_move()) {
+                possibleMoves = possibleFirstMovesBlack;
+                setFirst_moveToFalse();
+            }
+            else {
+                possibleMoves = possibleMovesBlack;
+            }
+        }
         for (int i = 0; i < possibleMoves.length; i = i + 2) {
             if (xDest == xcord + possibleMoves[i] && yDest == ycord + possibleMoves[i + 1]) {
                 return true;
