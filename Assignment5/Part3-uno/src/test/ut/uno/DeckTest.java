71a98d48fcfff0c0e8fcf975b63798c194bd0b22
Dominic Schmidli <dominicschmidli2510@gmail.com>
2019-12-11 14:32:58 +0100

    changes on DeckTest.java from the uno game

--- Assignment5/Part3-uno/src/test/ut/uno/DeckTest.java
+++ Assignment5/Part3-uno/src/test/ut/uno/DeckTest.java
@@ -1,4 +1,45 @@
 package test.ut.uno;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class DeckTest {
+    private static Deck d;
+
+    @BeforeClass
+    public static void setUp() {
+        d = new Deck();
+
+    }
+
+    @Test
+    public void testDrawCard() {
+        Card card = new Card(Color.Blue, 7);
+        d.addCardToDeck(card);
+        d.drawCard();
+
+
+    }
+
+    @Test
+    public void testGetDeck() {
+
+    }
+
+    @Test
+    public void testShuffle() {
+
+    }
+
+    @Test
+    public void testAddCardToDeck() {
+
+    }
+
+    @Test
+    public void testRemoveCardOfDeck() {
+
+    }
+
+
 }
